c354b79a4616cd64bb46845ea878ea0500e8419e
Le Pham <phamtle724@gmail.com>
2022-09-12 12:32:28 -0700

    getShowsByTerm

--- tvmaze.ts
+++ tvmaze.ts
@@ -13,37 +13,80 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm(term) {
+async function getShowsByTerm(term: string) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  const response: { data: []; } =
+    await axios.get('http://api.tvmaze.com/search/shows',
+      { params: { q: term } });
+
+  interface ShowInterface {
+    id: number | string;
+    name: string;
+    summary: string;
+    image: { original: string; } | null;
+  }
+
+  const showList = response.data.map(
+    function (x: {
+      show: ShowInterface;
+    }) {
+      let fallBackImage = "";
+      if (x.show.image === null) {
+        fallBackImage = "https://tinyurl.com/tv-missing";
+      }
+      else {
+        fallBackImage = x.show.image.original;
+      }
+
+      const result: ShowInterface = {
+        id: x.show.id,
+        name: x.show.name,
+        summary: x.show.summary,
+        image: {original: fallBackImage}
+      };
+      return result;
+    });
+  return showList;
+
+
+
+  // return [
+  //   {
+  //     id: 1767,
+  //     name: "The Bletchley Circle",
+  //     summary:
+  //       `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
+  //          women with extraordinary skills that helped to end World War II.</p>
+  //        <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
+  //          normal lives, modestly setting aside the part they played in
+  //          producing crucial intelligence, which helped the Allies to victory
+  //          and shortened the war. When Susan discovers a hidden code behind an
+  //          unsolved murder she is met by skepticism from the police. She
+  //          quickly realises she can only begin to crack the murders and bring
+  //          the culprit to justice with her former friends.</p>`,
+  //     image:
+  //         "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+  //   }
+  // ]
 }
 
 
 /** Given list of shows, create markup for each and to DOM */
 
-function populateShows(shows) {
+function populateShows(shows: []) {
   $showsList.empty();
+
+  interface ShowInterface {
+    id: number | string;
+    name: string;
+    summary: string;
+    image: { original: string; } | null;
+  }
+
 
   for (let show of shows) {
     const $show = $(
-        `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+      `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
               src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
@@ -60,7 +103,8 @@ function populateShows(shows) {
        </div>
       `);
 
-    $showsList.append($show);  }
+    $showsList.append($show);
+  }
 }
 
 
@@ -68,18 +112,18 @@ function populateShows(shows) {
  *    Hide episodes area (that only gets shown if they ask for episodes)
  */
 
-async function searchForShowAndDisplay() {
-  const term = $("#searchForm-term").val();
-  const shows = await getShowsByTerm(term);
+// async function searchForShowAndDisplay() {
+//   const term = $("#searchForm-term").val();
+//   const shows = await getShowsByTerm(term);
 
-  $episodesArea.hide();
-  populateShows(shows);
-}
+//   $episodesArea.hide();
+//   populateShows(shows);
+// }
 
-$searchForm.on("submit", async function (evt) {
-  evt.preventDefault();
-  await searchForShowAndDisplay();
-});
+// $searchForm.on("submit", async function (evt) {
+//   evt.preventDefault();
+//   await searchForShowAndDisplay();
+// });
 
 
 /** Given a show ID, get from API and return (promise) array of episodes:
